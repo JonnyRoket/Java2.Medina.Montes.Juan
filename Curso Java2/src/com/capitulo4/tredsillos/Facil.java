f3e772fee2079199b6cbe8f6b9b125fbafd78d86
JonnyRoket <roket17@live.com.mx>
2014-06-16 18:17:49 -0500

    Clase de threads

---
+++ Curso Java2/src/com/capitulo4/tredsillos/Facil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.capitulo4.tredsillos;
+
+/**
+ *
+ * @author Jonny
+ * Clase de Threads
+ * Ahi 3 formas que veremos
+ * Ya creamos un packages com.capitulo4.tredsillos
+ * y creamos esta clase llamada facil
+ * veamos como crear la primera forma
+ */
+public class Facil {
+    
+}
